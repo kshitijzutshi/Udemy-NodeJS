83ca6880186dab3341b5caa52aba37d08475e7ea
kshitijzutshi <kshitij.zutshi@gmail.com>
2019-11-30 20:27:27 +0530

    added chalk package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,6 @@
 //const add = require('./utils.js')
+const validator = require('validator')
+const chalk =  require('chalk')
 const getNote = require('./notes.js')
 
 const msg = getNote()
@@ -6,5 +8,11 @@ const msg = getNote()
 //const sum= add(4,-1)
 //console.log(sum)
 console.log(msg)
+// Trying out various methods in validator module
+console.log(validator.isEmail('johndoe@example.com'))
+console.log(validator.isURL('https://mead.io'))
+// Trying out the chalk package
+console.log(chalk.green('Success! chalk is working!'))
+console.log(chalk.green('I am a green line ' + chalk.blue.underline.bold('with a blue substring') + ' that becomes green again!'))
 
 console.log('Testing Git!!!!')
