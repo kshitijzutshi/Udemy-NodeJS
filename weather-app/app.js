5c203587ae90caadda9e1e85cbe6f3670f919a07
kshitijzutshi <kshitij.zutshi@gmail.com>
2019-12-24 18:20:34 +0530

    updated app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,18 @@
-console.log("Starting")
+const request = require('request')
 
-//Inbuilt async function that allows us to run some code after a specific
-// time has passed
-setTimeout( () => {
-console.log('2 second timer')
+// For the URL, if we can a customized forcast like diff. units or lang changes, just add query at end of the url. LIKE
+// 'https:// ....?units=si&xyz=abc....' here after ? there is key=value pair
 
-},  2000)
+const url = 'https://api.darksky.net/forecast/c889472ce43e33aa808d84773a03bbbd/37.8267,-122.4233?units=si'
 
-setTimeout( () => {
-    console.log('0 second timer')
-    
-    },  0)
-    
-console.log("Stopping")
+request({url : url, json : true}, (error,response) => {
+
+// console.log(response)
+// TO parse the body part of the response from http request, WHICH wont be needed if added json: true in request options
+// const data = JSON.parse(response.body)
+// console.log(data)
+// To get a lot less info and get current weather info
+// console.log(data.currently)
+console.log('It is currently '+response.body.currently.temperature+' degrees out. There is a '+(response.body.currently.precipProbability)*100+ '% chance of '+response.body.currently.precipType+'.')
+// console.log(process.argv)
+})
